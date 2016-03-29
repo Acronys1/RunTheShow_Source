a7370889f40a432477e1d6ef07dfab0bd2231da1
simaydin <simaydin@miage21a14.dmiage.u-paris10.fr>
2016-03-29 15:22:11 +0200

    Ajout delete & update des users HTML & JS

--- RunTheShow_Frontend/src/main/resources/static/js/users/users.js
+++ RunTheShow_Frontend/src/main/resources/static/js/users/users.js
@@ -1,17 +1,61 @@
-angular.module('users', []).controller('users', function ($scope, $http) {
+angular.module('users', []).controller('users', function ($scope, $http, $window) {
 
+    $scope.editingUser = {};
+    $scope.user = {};
+    
     $scope.initFirst = function ()
     {
         $scope.updateData = $http.get('/resource/user/all').success(function (data) {
             $scope.users = data;
-        });
+            for (var i = 0; lenght = $scope.editingUser.length; i++) {
 
+                $scope.editingUser[users[i].id] = false;
+            }
+        });
+        
         $http.get('/resource/user/role/all').success(function (data) {
             $scope.roles = data;
         });
     }
 
-    $scope.user = {};
+    //Update & delete
+
+
+
+
+    $scope.modifyUser = function (user) {
+        $scope.userEdit = user;
+        $scope.editingUser[user.id] = true;
+    };
+
+    $scope.updateUser = function () {
+        var data = JSON.stringify({
+                id: $scope.userEdit.id,
+                login: $scope.userEdit.login,                
+                enabled: $scope.userEdit.enabled,
+                roles: [{id: $scope.userEdit.userRole.id}]
+        })
+            $http.put("/resource/user/update", data).success(function (data, status) {
+            $scope.response = data;
+            $scope.initFirst();
+        })
+        $scope.editingUser[$scope.userEdit.id] = false;
+    };
+    
+    $scope.deleteUser = function(){
+        var data = JSON.stringify({
+                id: $scope.userEdit.id
+        })
+            $http.delete("/resource/user/delete", data).success(function (data, status) {
+            $scope.response = data;
+            $scope.initFirst();
+        })
+        $scope.editingUser[$scope.userEdit.id] = false; 
+        $scope.editingUser[$scope.userEdit.id] = false;
+    };
+    
+    //////
+
 
     $scope.addUser = function () {
 
