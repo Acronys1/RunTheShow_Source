b8ebd266accb6da6d891341802b92a0b18ac2199
simaydin <simaydin@miage21a14.dmiage.u-paris10.fr>
2016-05-10 14:14:49 +0200

    Artist presentation: css in progress

--- RunTheShow_Frontend/src/main/resources/static/js/artist_presentation/artist_presentation.js
+++ RunTheShow_Frontend/src/main/resources/static/js/artist_presentation/artist_presentation.js
@@ -1,4 +1,73 @@
 angular.module('artist_presentation', []).controller('artist_presentation', function ($scope, $http, $rootScope, $window) {
-    
+
+    $scope.artist = {};
+
+    $scope.initArtistPresentation = function () {
+        $http.get("/resource/user/current").success(function (data, status) {
+            $scope.artist = data;
+            if ($scope.artist.roles[0] == "ROLE_ARTISTE") {
+                $scope.errorMessage.init = "OKKKKKKKKKKKKKK";
+            }
+        }).error(function (data, status) { // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.errorMessage.init = "Pas un artiste.";
+        });
+
+
+    }
+
 });
 
+artist_presentation.directive('udatepicker', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModCtrl) {
+            $(function () {
+                var min = new Date();
+                var myHours = min.getHours();
+                min.setHours(myHours + 6)
+
+                element.datetimepicker({
+                    format: 'DD/MM/YYYY HH:mm',
+                    minDate: min,
+                    calendarWeeks: true
+                });
+                element.on("dp.hide", function (e) {
+
+
+                    if (attrs.id.indexOf("debut") != -1)
+                    {
+                        //alert("Check in value = " + attrs.id);
+                        ngModCtrl.$setViewValue(document.getElementById('datedebut').value);
+                        //$("#checkout").data("DateTimePicker").minDate(e.date); 
+                    }
+                    else
+                    {
+                        //alert("Check out value = " + attrs.id + "   CPT = " + cpt);
+                        ngModCtrl.$setViewValue(document.getElementById('datefin').value);
+                    }
+
+
+                });
+
+                element.on("dp.change", function (e) {
+
+                    if (attrs.id.indexOf("debut") != -1)
+                    {
+                        //alert("Check in value = " + attrs.id);
+                        ngModCtrl.$setViewValue(document.getElementById('datedebut').value);
+                        //$("#checkout").data("DateTimePicker").minDate(e.date); 
+                    }
+                    else
+                    {
+                        //alert("Check out value = " + attrs.id + "   CPT = " + cpt);
+                        ngModCtrl.$setViewValue(document.getElementById('datefin').value);
+                        $("#datedebut").data("DateTimePicker").maxDate(e.date);
+                    }
+
+                });
+            });
+        }
+    }
+});
